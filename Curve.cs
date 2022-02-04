ca8ba9cad672ef3054c2af83a4f3fb80359e76af
horcaseb <46980001+horcaseb@users.noreply.github.com>
2022-02-04 10:37:11 -0500

    Add files via upload

---
+++ Curve.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curva;
+    [SerializeField] float duracion = 5f;
+    Light luz;
+    float t = 0;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        luz=GetComponent<Light>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        luz.intensity = 10 * curva.Evaluate(t /duracion);
+        t += Time.deltaTime;
+    }
+}
